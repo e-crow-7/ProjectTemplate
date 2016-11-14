6eb719b7584a137cb31b078c1b516daa56e9683c
e-crow-7 <e-crow-7@users.noreply.github.com>
2016-11-13 21:43:57 -0600

    Added standard hello world cpp and cmakelists.

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*!
+ * @file Main.cpp
+ * @brief Standard Hello World application.
+ *
+ * @Author ecrow
+ * @version 1.0
+ */
+
+#include <ostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	return 1;
+}
